40f897d603d304c5a3333aac34cedbe459001094
github username <etellezp@gmail.com>
2019-08-03 12:56:29 -0400

    Fetch messages from the database and set the state of Wall with the data, then pass each message as a prop to the Message component and render each message to the DOM

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import './App.css'
 function App() {
   return (
     <div className="App">
-      <h2>Test</h2>
       <Wall />
     </div>
   );
--- src/Wall.js
+++ src/Wall.js
@@ -1,9 +1,37 @@
 import React from 'react'
+import Message from './Message'
 
 class Wall extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      messages: []
+    }
+  }
+
+  componentDidMount() {
+    let data = {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }
+
+    fetch("http://localhost:3001/api/v1/messages", data)
+      .then(response => response.json())
+      .then(response => this.setState({
+        messages: response.data
+      }))
+  }
+
   render() {
+    let message = this.state.messages.map(message => <Message key={message.id} message={message.attributes.content}/>)
     return (
-      "Hello from wall" 
+      <div>
+        <h1>Welcome to the Wall APP</h1>
+        {message}
+      </div>
     )
   }
 }
