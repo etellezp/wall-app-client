eea11335e96679e9863002457862aaacc2b226e3
github username <etellezp@gmail.com>
2019-08-04 17:32:37 -0400

    Added react router for better UI and handling of routes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from 'react'
 import Wall from './Wall'
 import Login from './Login'
 import './App.css'
+import { Switch, Route, NavLink } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <Wall />
-      <Login />
+      <div>
+        <NavLink exact to="/">Wall |</NavLink>
+        <NavLink exact to="/login">Login |</NavLink>
+      </div>
+
+      <Switch>
+        <Route exact path='/' component={Wall} />
+        <Route exact path='/login' component={Login} />
+      </Switch>
     </div>
   );
 }
