a17676449bd6bb54fb26d3136a8383b54fe24e7c
github username <etellezp@gmail.com>
2019-08-04 18:33:26 -0400

    Get a current user in App componentDidMount if there is already a user

--- src/App.js
+++ src/App.js
@@ -4,20 +4,58 @@ import Login from './Login'
 import './App.css'
 import { Switch, Route, NavLink } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <div>
-        <NavLink exact to="/">Wall |</NavLink>
-        <NavLink exact to="/login">Login |</NavLink>
-      </div>
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentUser: null,
+      loginForm: {
+        email: "",
+        password: ""
+      }
+    }
+  }
 
-      <Switch>
-        <Route exact path='/' component={Wall} />
-        <Route exact path='/login' component={Login} />
-      </Switch>
-    </div>
-  );
+  componentDidMount() {
+    let data = {
+      method: 'GET',
+      credentials: "include",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }
+
+    fetch("http://localhost:3001/api/v1/get_current_user", data)
+      .then(response => response.json())
+      .then(response => {
+        if (response.error) {
+          alert(response.error)
+        } else {
+          this.setState({
+            currentUser: response
+          })
+        }
+      })
+      .catch(console.log)
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+        <div>
+          <NavLink exact to="/">Wall |</NavLink>
+          <NavLink exact to="/login">Login |</NavLink>
+        </div>
+
+        <Switch>
+          <Route exact path='/' component={Wall} />
+          <Route exact path='/login' component={Login} />
+        </Switch>
+      </div>
+    )
+  }
 }
 
 export default App;
