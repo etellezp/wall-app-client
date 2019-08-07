683f3218a1304226a70a7cfb2f3f55b5aa10f7da
github username <etellezp@gmail.com>
2019-08-07 11:44:22 -0400

    Added key to each message

--- src/UserCard.js
+++ src/UserCard.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const UserCard = ({ user }) => {
+  let message = user.data.attributes.messages.map(m => <p key={m.id}>{m.content}</p>)
   return (
     <div>
       <h1>Hello {user.data.attributes.username}</h1>
+      {message.length > 0 ? message : "You have no messages"}
     </div>
   )
 }
